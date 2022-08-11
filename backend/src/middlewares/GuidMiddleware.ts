20345ce7b933140242916e793584b55587dd38fa
valdeci <moura97.valdeci@gmail.com>
2022-08-11 16:05:40 -0300

    feat: create guid and product middlewares

---
+++ backend/src/middlewares/GuidMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import { uuidSchema } from '../joiSchemas/uuid';
+
+export default class GuidMiddleware {
+  public validateGuid = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { id } = req.params;
+    const { error } = uuidSchema.validate(id);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+}
---
+++ backend/src/middlewares/ProductMiddleware.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import { productName, productPrice, productQuantity } from '../joiSchemas/product';
+
+export default class ProductMiddleware {
+  public validateName = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = productName.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  };
+
+  public validatePrice = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = productPrice.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+
+  public validateQuantity = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = productQuantity.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+}
