96c6ca2c3bd406395a0fd753897a166c802a949c
valdeci <moura97.valdeci@gmail.com>
2022-08-10 22:34:51 -0300

    feat(server): add product controller to server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,15 @@
+import CustomRouter from './routes';
 import App from './app';
+import ProductController from './controllers/ProductController';
+import { Product } from './types/ProductType';
 
 const server = new App();
+
+const productController = new ProductController();
+
+const productRouter = new CustomRouter<Product>();
+productRouter.addRoute(productController, productController.route);
+
+server.addRouter(productRouter.router);
 
 export default server;
