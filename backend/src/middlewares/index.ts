5546e1bc5a385d8cae016723f3b37e53fda54dd1
valdeci <moura97.valdeci@gmail.com>
2022-08-11 16:32:51 -0300

    feat(middleware): create global error handler

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpException from '../exceptions/HttpException';
+
+export default class GlobalMiddleware {
+  public error = (
+    err: HttpException,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ): Response => {
+    const code = Number(err.status) || 500;
+    const message = err.message || 'Something went wrong.';
+    return res.status(code).json({ message });
+  }
+}
