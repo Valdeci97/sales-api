a487f7dd7b83ad1769d7621ba6262b2af41e6bfa
valdeci <moura97.valdeci@gmail.com>
2022-08-12 17:41:35 -0300

    refactor(routes): remove methods with redudant behaviour

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -9,13 +9,6 @@ export default class CustomRouter<T> {
     this.router = Router();
   }
 
-  public readRoute(
-    controller: Controller<T>,
-    route: string = controller.route
-  ): void {
-    this.router.get(route, controller.read);
-  }
-
   public readOneRoute(
     controller: Controller<T>,
     route: string = controller.route,
@@ -24,65 +17,41 @@ export default class CustomRouter<T> {
     this.router.get(`${route}/:id`, ...middlewares, controller.readOne);
   }
 
-  public createRoute(
-    controller: Controller<T>,
-    route: string = controller.route,
-    ...middlewares: RequestHandler[]
-  ): void {
-    this.router.post(route, ...middlewares, controller.create);
-  }
-
-  public updateRoute(
-    controller: Controller<T>,
-    route: string = controller.route,
-    ...middlewares: RequestHandler[]
-  ): void {
-    this.router.put(`${route}/:id`, ...middlewares, controller.update);
-  }
-
-  public deleteRoute(
-    controller: Controller<T>,
-    route: string = controller.route,
-    ...middlewares: RequestHandler[]
-  ): void {
-    this.router.delete(`${route}/:id`, ...middlewares, controller.delete);
-  }
-
   public addGetRoute(
-    reqFunc: RequestHandler,
     route: string,
+    reqFunc: RequestHandler,
     ...middlewares: RequestHandler[]
   ): void {
     this.router.get(route, ...middlewares, reqFunc);
   }
 
   public addPostRoute(
-    reqFunc: RequestHandler,
     route: string,
+    reqFunc: RequestHandler,
     ...middlewares: RequestHandler[]
   ): void {
     this.router.post(route, ...middlewares, reqFunc);
   }
 
   public addPutRoute(
-    reqFunc: RequestHandler,
     route: string,
+    reqFunc: RequestHandler,
     ...middlewares: RequestHandler[]
   ): void {
     this.router.put(route, ...middlewares, reqFunc);
   }
 
   public addPatchRoute(
-    reqFunc: RequestHandler,
     route: string,
+    reqFunc: RequestHandler,
     ...middlewares: RequestHandler[]
   ): void {
     this.router.patch(route, ...middlewares, reqFunc);
   }
 
   public addDeleteRoute(
-    reqFunc: RequestHandler,
     route: string,
+    reqFunc: RequestHandler,
     ...middlewares: RequestHandler[]
   ): void {
     this.router.delete(route, ...middlewares, reqFunc);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,15 +4,17 @@ import ProductController from './controllers/ProductController';
 import GuidMiddleware from './middlewares/GuidMiddleware';
 import ProductMiddleware from './middlewares/ProductMiddleware';
 import { Product } from './types/ProductType';
+import UserController from './controllers/UserController';
+import { User } from './types/UserType';
 
 const server = new App();
 const guidMiddleware = new GuidMiddleware();
 
 const productController = new ProductController();
 const productMiddleware = new ProductMiddleware();
-
 const productRouter = new CustomRouter<Product>();
-productRouter.readRoute(productController);
+
+productRouter.addGetRoute(productController.route, productController.read);
 
 productRouter.readOneRoute(
   productController,
@@ -20,30 +22,44 @@ productRouter.readOneRoute(
   guidMiddleware.validateGuid
 );
 
-productRouter.createRoute(
-  productController,
+productRouter.addPostRoute(
   productController.route,
+  productController.create,
   productMiddleware.validateName,
   productMiddleware.validatePrice,
   productMiddleware.validateQuantity
 );
 
-productRouter.updateRoute(
-  productController,
-  productController.route,
+productRouter.addPutRoute(
+  `${productController.route}/:id`,
+  productController.update,
   productMiddleware.validateName,
   productMiddleware.validatePrice,
   productMiddleware.validateQuantity,
   guidMiddleware.validateGuid
 );
 
-productRouter.deleteRoute(
-  productController,
-  productController.route,
+productRouter.addDeleteRoute(
+  `${productController.route}/:id`,
+  productController.delete,
   guidMiddleware.validateGuid
 );
 
+const userController = new UserController();
+const userRouter = new CustomRouter<User>();
+
+userRouter.addGetRoute(userController.route, userController.read);
+
+userRouter.readOneRoute(userController);
+
+userRouter.addPostRoute(userController.route, userController.create);
+
+userRouter.addPutRoute(`${userController.route}/:id`, userController.update);
+
+userRouter.addDeleteRoute(`${userController.route}/:id`, userController.delete);
+
 server.addRouter(productRouter.router);
+server.addRouter(userRouter.router);
 
 server.addErrorMiddleware();
 
