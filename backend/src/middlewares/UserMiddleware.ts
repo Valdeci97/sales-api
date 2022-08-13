e1b10387913406f2b983a1238afee25a2a696eef
valdeci <moura97.valdeci@gmail.com>
2022-08-12 23:47:54 -0300

    feat(user): create middleware class

---
+++ backend/src/middlewares/UserMiddleware.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import { userEmail, username, userPassword } from '../joiSchemas/user';
+
+export default class UserMiddleware {
+  public validateName = (
+    req: Request,
+    res: Response,
+    next: NextFunction  
+  ): Response | void => {
+    const { error } = username.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+
+  public validateEmail = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userEmail.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+
+  public validatePassword = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userPassword.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+}
