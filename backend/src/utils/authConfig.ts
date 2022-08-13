0dc8f215af3870706d7dadce8698f7d3578c19e2
valdeci <moura97.valdeci@gmail.com>
2022-08-13 16:23:12 -0300

    feat: create jsonwebtoken class e config its properties

---
+++ backend/src/utils/authConfig.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { hashSync } from 'bcryptjs';
+
+const hashSecret = (secret: string, salts: number = 10) =>
+  hashSync(secret, salts);
+
+const secret = '759b74ce43947f5f4c91aeddc3e5bad3';
+
+const SECRET = process.env.SECRET || hashSecret(secret);
+
+const authConfig = {
+  secret: SECRET,
+  expiresIn: '1d',
+};
+
+export default authConfig;
---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
+
+import authConfig from './authConfig';
+
+export default class JsonWebToken {
+  public static generate(obj: string | object | Buffer): string {
+    return sign(obj, authConfig.secret, {
+      algorithm: 'HS512',
+      expiresIn: authConfig.expiresIn,
+    });
+  }
+
+  public static decode(token: string): string | JwtPayload {
+    return verify(token, authConfig.secret);
+  }
+}
