aa548d3c12aece5c59ffeb2a2fdf721041ba88f0
valdeci <moura97.valdeci@gmail.com>
2022-08-13 16:25:08 -0300

    feat: create login controller

---
+++ backend/src/controllers/LoginController.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import HttpException from '../exceptions/HttpException';
+import UserService from '../services/UserService';
+
+export default class LoginController {
+  service = new UserService();
+
+  public login = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response | void> => {
+    const { body } = req;
+    try {
+      const [code, message, user] = await this.service.login(body);
+      if (message.length > 0) return res.status(code).json({ message });
+      return res.status(code).json({ user });
+    } catch (error) {
+      next(new HttpException());
+    }
+  };
+}
