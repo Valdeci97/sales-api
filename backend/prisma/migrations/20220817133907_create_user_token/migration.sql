8f411e36d2fb2294e1bba046e06300475ca11ce9
valdeci <moura97.valdeci@gmail.com>
2022-08-17 15:45:45 -0300

    build(prisma): insert generated_at field at user_tokens table

--- backend/prisma/migrations/20220817133907_create_user_token/migration.sql
+++ backend/prisma/migrations/20220817133907_create_user_token/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "user_tokens" (
     "id" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
+    "generated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "user_tokens_pkey" PRIMARY KEY ("id")
 );
