af1dcd7e92fb1ed37e265144c10058ca5a5ea9c3
valdeci <moura97.valdeci@gmail.com>
2022-08-17 20:48:01 -0300

    refactor(password service): join user token service at password service

--- backend/src/services/PasswordService.ts
+++ backend/src/services/PasswordService.ts
@@ -1,10 +1,12 @@
 /* eslint-disable class-methods-use-this */
-import { PrismaClient } from '@prisma/client';
+/* eslint-disable camelcase */
+import { PrismaClient, UserToken, User } from '@prisma/client';
 import { hash } from 'bcryptjs';
 import { addHours, isAfter } from 'date-fns';
 import { database } from '../database';
 import { ResetPassword } from '../types/ResetPassword';
 import { TokenResponse } from '../types/TokenResponse';
+import { DbResponse } from '../types/DbResponse';
 
 export default class PasswordService {
   private model: PrismaClient;
@@ -19,6 +21,13 @@ export default class PasswordService {
     this.model = model;
   }
 
+  public async findUser(id: string): Promise<User | null> {
+    const user = await this.model.user.findFirst({
+      where: { id },
+    });
+    return user;
+  }
+
   public async resetPassword({
     token,
     password,
@@ -30,20 +39,13 @@ export default class PasswordService {
       return this.createTokenResponse(400, 'Token does not exist!');
     }
 
-    const user = await this.model.user.findFirst({
-      where: { id: userToken.user_id },
-    });
+    const user = await this.findUser(userToken.user_id);
     if (!user) return this.createTokenResponse(404, 'User not found!');
     if (this.isInvalidToken(userToken.generated_at)) {
       return this.createTokenResponse(400, 'Invalid token!');
     }
     await this.updatePassword(user.id, password);
     return this.createTokenResponse(200, 'Password updated successfully!');
-  }
-
-  private isInvalidToken(date: Date | number): boolean {
-    const compare = addHours(date, this.LIMIT_TO_VALIDATE_A_TOKEN);
-    return isAfter(this.date, compare);
   }
 
   private async updatePassword(id: string, password: string): Promise<void> {
@@ -54,11 +56,38 @@ export default class PasswordService {
     });
   }
 
+  public async createToken(userId: string): Promise<DbResponse> {
+    const userToken = await this.model.userToken.create({
+      data: {
+        user_id: userId,
+      },
+    });
+    return this.createDbResponse(201, '', userToken);
+  }
+
+  private isInvalidToken(date: Date | number): boolean {
+    const compare = addHours(date, this.LIMIT_TO_VALIDATE_A_TOKEN);
+    return isAfter(this.date, compare);
+  }
+
   private createTokenResponse(
     status: number,
     message: string = '',
     data: ResetPassword = { token: '', password: '' }
   ): TokenResponse {
+    return [status, message, data];
+  }
+
+  private createDbResponse(
+    status: number,
+    message: string = '',
+    data: UserToken = {
+      id: '',
+      token: '',
+      user_id: '',
+      generated_at: new Date(),
+    }
+  ): DbResponse {
     return [status, message, data];
   }
 }
