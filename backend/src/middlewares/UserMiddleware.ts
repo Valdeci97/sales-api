75217a819c39ab89a11820afeaa6e1b871a9b346
valdeci <moura97.valdeci@gmail.com>
2022-08-21 17:22:14 -0300

    feat(user optional fields): add e-mail and password optional middlewares

--- backend/src/middlewares/UserMiddleware.ts
+++ backend/src/middlewares/UserMiddleware.ts
@@ -4,6 +4,8 @@ import {
   username,
   userPassword,
   userPasswordConfirmation,
+  userOptionalEmail,
+  userOptionalPassword,
 } from '../utils/joiSchemas/user';
 
 export default class UserMiddleware {
@@ -52,6 +54,32 @@ export default class UserMiddleware {
     next: NextFunction
   ): Response | void => {
     const { error } = userPasswordConfirmation.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+
+  public validateOptionalEmail = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userOptionalEmail.validate(req.body);
+    if (error) {
+      const [code, message] = error.message.split('/');
+      return res.status(Number(code)).json({ message });
+    }
+    next();
+  }
+
+  public validateOptionalPassword = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response | void => {
+    const { error } = userOptionalPassword.validate(req.body);
     if (error) {
       const [code, message] = error.message.split('/');
       return res.status(Number(code)).json({ message });
