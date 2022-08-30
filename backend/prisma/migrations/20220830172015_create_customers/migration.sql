532865792f47c21e5d9bb624978d7c4b3be45c24
valdeci <moura97.valdeci@gmail.com>
2022-08-30 14:21:09 -0300

    feat(customer): create table

---
+++ backend/prisma/migrations/20220830172015_create_customers/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "curstomers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "curstomers_pkey" PRIMARY KEY ("id")
+);
