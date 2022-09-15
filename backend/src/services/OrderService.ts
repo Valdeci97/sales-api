fd71a37d185ec5428edcd5cc60bcb8179517036f
valdeci <moura97.valdeci@gmail.com>
2022-09-15 18:47:55 -0300

    feat(order service): update products quantities after create an order

--- backend/src/services/OrderService.ts
+++ backend/src/services/OrderService.ts
@@ -1,4 +1,5 @@
-import OrderProductsValidator from '../facade/OrderProductsValidator';
+import { Order, Product } from '@prisma/client';
+import OrderProductsHandler from '../facade/OrderProductsHandler';
 import CustomerModel from '../models/CustomerModel';
 import OrderProductsModel from '../models/OrderProducts';
 import OrderModel from '../models/Orders';
@@ -12,25 +13,25 @@ export default class OrderService {
 
   private orderProductsModel: OrderProductsModel;
 
-  private orderProductsValidator: OrderProductsValidator;
+  private orderProductsHandler: OrderProductsHandler;
 
   constructor(
     customerModel: CustomerModel = new CustomerModel(),
     orderModel: OrderModel = new OrderModel(),
     orderProductsModel: OrderProductsModel = new OrderProductsModel(),
-    orderProductsValidator: OrderProductsValidator = new OrderProductsValidator()
+    orderProductsHandler: OrderProductsHandler = new OrderProductsHandler()
   ) {
     this.customerModel = customerModel;
     this.orderModel = orderModel;
     this.orderProductsModel = orderProductsModel;
-    this.orderProductsValidator = orderProductsValidator;
+    this.orderProductsHandler = orderProductsHandler;
   }
 
   public async create({ customerId, products }: OrderRequest) {
     const customer = await this.customerModel.listById(customerId);
     if (!customer) throw new HttpException(404, 'Customer not found');
     const isAvailableQuantity =
-      await this.orderProductsValidator.isAvailableQuantity(products);
+      await this.orderProductsHandler.isAvailableQuantity(products);
     if (!isAvailableQuantity) {
       throw new HttpException(
         400,
@@ -38,6 +39,14 @@ export default class OrderService {
       );
     }
     const order = await this.orderModel.create(customer.id);
+    await this.createOrderRelation(products, order);
+    await this.orderProductsHandler.updateOrderProductsQuantity(products);
+  }
+
+  private async createOrderRelation(
+    products: Product[],
+    order: Order
+  ): Promise<void> {
     await Promise.all(
       products.map(async (product) => {
         const orderRelation = { orderId: order.id, product };
