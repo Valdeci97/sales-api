a63c7da97113ce1ebd2462c8b492938c21e7d703
valdeci <moura97.valdeci@gmail.com>
2022-09-17 17:21:57 -0300

    refactor(password routes): add token middleware validation

--- backend/src/routes/passwordRoutes.ts
+++ backend/src/routes/passwordRoutes.ts
@@ -1,12 +1,14 @@
 import CustomRouter from '.';
 import PasswordController from '../controllers/PasswordController';
 import GuidMiddleware from '../middlewares/GuidMiddleware';
+import TokenMiddleware from '../middlewares/Token';
 import UserMiddleware from '../middlewares/UserMiddleware';
 
 const passwordController = new PasswordController();
 const userMiddleware = new UserMiddleware();
 const guidMiddleware = new GuidMiddleware();
 const passwordRouter = new CustomRouter();
+const tokenMiddleware = new TokenMiddleware();
 
 passwordRouter.addPostRoute(
   '/password/forgot',
@@ -17,6 +19,7 @@ passwordRouter.addPostRoute(
 passwordRouter.addPostRoute(
   '/password/reset',
   passwordController.resetPassword,
+  tokenMiddleware.validate,
   guidMiddleware.validateBodyGuid,
   userMiddleware.validatePassword,
   userMiddleware.validatePasswordConfirm
