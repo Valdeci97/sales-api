0a4c3a7e32437ee3976513704923d33f9ed79d5f
valdeci <moura97.valdeci@gmail.com>
2022-09-18 00:19:03 -0300

    feat(order routes): add middleware

--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -1,17 +1,21 @@
 import CustomRouter from '.';
 import OrderController from '../controllers/OrderController';
 import GuidMiddleware from '../middlewares/GuidMiddleware';
+import OrderMiddleware from '../middlewares/OrderMiddleware';
 import TokenMiddleware from '../middlewares/Token';
 
 const orderRouter = new CustomRouter();
 const orderController = new OrderController();
 const guidMiddleware = new GuidMiddleware();
 const tokenMiddleware = new TokenMiddleware();
+const orderMiddleware = new OrderMiddleware();
 
 orderRouter.addPostRoute(
   '/orders',
   orderController.create,
-  tokenMiddleware.validate
+  tokenMiddleware.validate,
+  orderMiddleware.validateCustomerId,
+  orderMiddleware.validateProductsArray
 );
 orderRouter.addGetRoute(
   '/orders/:id',
