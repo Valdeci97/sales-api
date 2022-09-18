d8a686ee640f9cc9623a54ffbfa6d588339ebb37
valdeci <moura97.valdeci@gmail.com>
2022-09-18 00:25:09 -0300

    refactor(customer routes): apply token and user middleware validations

--- backend/src/routes/customerRoutes.ts
+++ backend/src/routes/customerRoutes.ts
@@ -1,29 +1,44 @@
 import CustomRouter from '.';
 import CustomerController from '../controllers/CustomerController';
 import GuidMiddleware from '../middlewares/GuidMiddleware';
+import TokenMiddleware from '../middlewares/Token';
+import UserMiddleware from '../middlewares/UserMiddleware';
 
 const customerController = new CustomerController();
 const guidMiddleware = new GuidMiddleware();
 const customerRouter = new CustomRouter();
+const userMiddleware = new UserMiddleware();
+const tokenMiddleware = new TokenMiddleware();
 
-customerRouter.addGetRoute(customerController.route, customerController.read);
+customerRouter.addGetRoute(
+  customerController.route,
+  customerController.read,
+  tokenMiddleware.validate
+);
 customerRouter.addGetRoute(
   `${customerController.route}/:id`,
   customerController.readOne,
+  tokenMiddleware.validate,
   guidMiddleware.validateGuid
 );
 customerRouter.addPostRoute(
   customerController.route,
-  customerController.create
+  customerController.create,
+  tokenMiddleware.validate,
+  guidMiddleware.validateBodyGuid,
+  userMiddleware.validateName,
+  userMiddleware.validateEmail
 );
 customerRouter.addPutRoute(
   `${customerController.route}/:id`,
   customerController.update,
+  tokenMiddleware.validate,
   guidMiddleware.validateGuid
 );
 customerRouter.addDeleteRoute(
   `${customerController.route}/:id`,
   customerController.delete,
+  tokenMiddleware.validate,
   guidMiddleware.validateGuid
 );
 
